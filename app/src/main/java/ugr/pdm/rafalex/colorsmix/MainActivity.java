a7f57c28bb92e276cbaa8c7f726d1d576ed3f478
Alejandro Guerrero Martínez <alejandroguerrero.gr@gmail.com>
2017-04-07 12:56:01 +0200

    Inicio de la aplicación, botón de ayuda en el menú.

--- app/src/main/java/ugr/pdm/rafalex/colorsmix/MainActivity.java
+++ app/src/main/java/ugr/pdm/rafalex/colorsmix/MainActivity.java
@@ -1,13 +1,73 @@
 package ugr.pdm.rafalex.colorsmix;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Datos de la vista
+    private Button start;
+
+    //Datos para dialogos
+    private AlertDialog menuDialog;
+    private AlertDialog.Builder helpDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.start_button);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Inicia la siguiente acitividad.
+                //Intent i = new Intent(MainMenuActivity.this, PlayActivity.class);
+                //startActivity(i);
+            }
+        });
+
+        //Crea los dialogos
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.help_dialog, null);
+
+        helpDialog = new AlertDialog.Builder(this, R.style.DialogTheme)
+                .setView(dialogView)
+                .setTitle(R.string.menu_help)
+                .setNeutralButton(R.string.ok_button,null);
+
+        menuDialog = helpDialog.create();
+    }
+
+    //Función onCreateOptionMenu, para añadir el estilo de nuestro action_bar
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu); // set your file name
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Función onOptionItemSelected, para definir el funcionamiento de las opciones del action_bar
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+
+        if (item.getItemId() == R.id.help) {
+
+            menuDialog.show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
